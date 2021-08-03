75296687ef73bcbb97d76f718f23d0518252502a
Dwe69 <wahyu5161.w5@gmail.com>
2021-08-03 20:42:46 +0800

    New Function and method

--- app/src/main/java/com/example/fakestore/activity/MainActivity.kt
+++ app/src/main/java/com/example/fakestore/activity/MainActivity.kt
@@ -20,7 +20,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        showProducts()
+//        showProducts()
+//        showProductsSingle()
+//        showProductsLimit()
+        showSortProducts()
+    }
+
+    private fun showSortProducts() {
+        rvProducts.setHasFixedSize(true)
+        rvProducts.layoutManager = LinearLayoutManager(this)
+        RetrofitStore.instance.getSortProducts( "id","desc").enqueue(object : Callback<ArrayList<ProductsResponse>>{
+            override fun onResponse(
+                call: Call<ArrayList<ProductsResponse>>,
+                response: Response<ArrayList<ProductsResponse>>
+            ) {
+                val responseCode = response.code().toString()
+                tvResponseCode.text = responseCode
+                response.body()?.let { list.addAll(it) }
+                val adapter = ProductsAdapter(list)
+                rvProducts.adapter = adapter
+            }
+
+            override fun onFailure(call: Call<ArrayList<ProductsResponse>>, t: Throwable) {
+                tvResponseCode.text = t.message
+            }
+
+        })
     }
 
     private fun showProducts() {
@@ -44,6 +69,51 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
 
+    private fun showProductsSingle() {
+        rvProducts.setHasFixedSize(true)
+        rvProducts.layoutManager = LinearLayoutManager(this)
+        RetrofitStore.instance.getProductsSingle(1).enqueue(object : Callback<ProductsResponse>{
+            override fun onResponse(
+                call: Call<ProductsResponse>,
+                response: Response<ProductsResponse>
+            ) {
+                val responseCode = response.code().toString()
+                tvResponseCode.text = responseCode
+                response.body()?.let { list.addAll(listOf(it)) }
+                val adapter = ProductsAdapter(list)
+                rvProducts.adapter = adapter
+            }
+
+            override fun onFailure(call: Call<ProductsResponse>, t: Throwable) {
+                tvResponseCode.text = t.message
+            }
+
+        })
+
+    }
+
+    private fun showProductsLimit() {
+        rvProducts.setHasFixedSize(true)
+        rvProducts.layoutManager = LinearLayoutManager(this)
+
+        RetrofitStore.instance.getProductsLimit("/products?limit=5").enqueue(object : Callback<ArrayList<ProductsResponse>>{
+            override fun onResponse(
+                call: Call<ArrayList<ProductsResponse>>,
+                response: Response<ArrayList<ProductsResponse>>
+            ) {
+                val responseCode = response.code().toString()
+                tvResponseCode.text = responseCode
+                response.body()?.let { list.addAll(it) }
+                val adapter = ProductsAdapter(list)
+                rvProducts.adapter = adapter
+            }
+
+            override fun onFailure(call: Call<ArrayList<ProductsResponse>>, t: Throwable) {
+                tvResponseCode.text = t.message
+            }
+
+        })
     }
 }
--- app/src/main/java/com/example/fakestore/network/Api.kt
+++ app/src/main/java/com/example/fakestore/network/Api.kt
@@ -1,10 +1,26 @@
 package com.example.fakestore.network
 
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface Api {
     @GET("products")
     fun getProducts(): Call<ArrayList<ProductsResponse>>
+
+    @GET("products")
+    fun getSortProducts(
+        @Query("order") order: String,
+        @Query("sort") sort: String
+    ): Call<ArrayList<ProductsResponse>>
+
+    @GET
+    fun getProductsSingle(@Url url: String): Call<ProductsResponse>
+
+    @GET("/products/{id}")
+    fun getProductsSingle(@Path("id") id: Int): Call<ProductsResponse>
+
+    @GET
+    fun getProductsLimit(@Url url: String): Call<ArrayList<ProductsResponse>>
+
+
 }
